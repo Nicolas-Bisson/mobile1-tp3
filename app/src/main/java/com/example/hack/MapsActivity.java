e627cf9ccdb27f963ae2bf4c5e5241140d04df0c
Nicolas Bisson <nicolasbisson14@gmail.com>
2019-03-09 10:01:11 -0500

    Marqueur afficher

--- app/src/main/java/com/example/hack/MapsActivity.java
+++ app/src/main/java/com/example/hack/MapsActivity.java
@@ -10,8 +10,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.SphericalUtil;
 
 import java.io.InputStream;
+import java.util.InvalidPropertiesFormatException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -22,14 +24,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        ParserCSV.Instance.Parse();
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        }
-
         try
         {
             final InputStream FICHIER = this.getResources().openRawResource(R.raw.bornes);
@@ -39,6 +33,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             System.out.println(e.toString());
         }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
     }
 
 
@@ -49,11 +50,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng quebec = new LatLng(46.829853, -71.254028);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(quebec));
         mMap.setMinZoomPreference(8);
-        int e = 0;
-        for (int i = 0; i < ParserCSV.Instance.bornes.size(); i++) {
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLatitude()), Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLongitude())))
-                    .title(ParserCSV.Instance.bornes.get(i).getNomBorne()));
-        }
+        for (int i = 1; i < ParserCSV.Instance.bornes.size(); i++) {
+            try {
+                Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLatitude());
+                if (SphericalUtil.computeDistanceBetween(new LatLng(mMap.getProjection().getVisibleRegion().nearLeft.latitude, mMap.getProjection().getVisibleRegion().nearLeft.longitude),
+                        new LatLng(Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLatitude()), Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLongitude()))) > 10)
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLatitude()), Double.parseDouble(ParserCSV.Instance.bornes.get(i).getLongitude())))
+                        .title(ParserCSV.Instance.bornes.get(i).getNomBorne()));
+            }
+            catch (NumberFormatException ex)
+            {
+            }
+            }
     }
 }
